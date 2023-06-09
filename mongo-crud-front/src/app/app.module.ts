13cfd2fd364780ecbe2cd93f7a03360525d49165
Andersson <a.cardenas.quiros@gmail.com>
2023-06-08 23:13:29 -0500

    Components, services and class created

--- mongo-crud-front/src/app/app.module.ts
+++ mongo-crud-front/src/app/app.module.ts
@@ -3,13 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
+//Components
+import { ListComponent } from './product/list.component';
+import { DetailComponent } from './product/detail.component';
+import { CreateComponent } from './product/create.component';
+import { UpdateComponent } from './product/update.component';
+
 //External
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    DetailComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
